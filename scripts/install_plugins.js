3795465431b88a4d1b4cf3893d66553561d2872d
lenteweb <webmaster@lenteweb.com>
2016-08-15 22:36:06 -0500

    Subir Master
    app demos

---
+++ scripts/install_plugins.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+//this hook installs all your plugins
+
+// add your plugins to this list--either 
+// the identifier, the filesystem location 
+// or the URL
+var pluginlist = [
+    "org.apache.cordova.device",
+    "org.apache.cordova.device-motion",
+    "org.apache.cordova.device-orientation",
+    "org.apache.cordova.geolocation",
+	"org.apache.cordova.inappbrowser",
+    "https://github.com/chrisekelley/AppPreferences/"
+];
+
+// no need to configure below
+
+var fs = require('fs');
+var path = require('path');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+function puts(error, stdout, stderr) {
+    sys.puts(stdout)
+}
+
+pluginlist.forEach(function(plug) {
+    exec("phonegap plugin add " + plug, puts);
+});
