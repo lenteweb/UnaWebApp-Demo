c8c243c1c20723c7443bad0e6e138a381a07bc0c
lenteweb <webmaster@lenteweb.com>
2016-08-17 14:32:20 -0500

    sms
    integracion de plugin y permisos

--- scripts/install_plugins.js
+++ scripts/install_plugins.js
@@ -12,6 +12,7 @@ var pluginlist = [
     "org.apache.cordova.geolocation",
 	"org.apache.cordova.inappbrowser",
     "https://github.com/chrisekelley/AppPreferences/",
+	"https://github.com/cordova-sms/cordova-sms-plugin.git ",
 	"$ phonegap local plugin add https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin.git"
 ];
 
